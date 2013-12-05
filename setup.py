6ac47da74550e11d22b28708226a0daa430a4b50
Florian Leitner <florian.leitner@gmail.com>
2013-12-05 10:36:17 +0100

    new standalone version

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+# TODO: MANIFEST.in with the .rst files in doc
+
+setup(
+    name='gnamed',
+    version='1',
+    license='GNU GPL v3',
+    author='Florian Leitner',
+    author_email='florian.leitner@gmail.com',
+    url='https://github.com/fnl/gnamed',
+    description='loading and maintaining a gene name database',
+    long_description=open('README.rst').read(),
+    package_dir={'': 'src'},
+    install_requires=[
+        'sqlalchemy >= 0.8',
+        'progress_bar = 1',
+    ],
+    packages=[
+        'gnamed',
+        'gnamed.parsers',
+    ],
+    scripts=[
+        'scripts/gnamed',
+    ],
+    data_files=[
+        ('/usr/local/share/man/man1', ['gnamed.1']),
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+)
---
+++ src/gnamed/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. py:module:: gnamed
+   :synopsis: Modules to bootstrap a gene/protein name/reference repository.
+
+.. moduleauthor:: Florian Leitner <florian.leitner@gmail.com>
+.. License: GNU GPL v3 (http://www.gnu.org/licenses/gpl.html)
+"""
+
+__author__ = "Florian Leitner"
